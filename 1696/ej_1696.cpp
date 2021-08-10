e7f2421738bd262dbc5625997cb139dd307e8995
brobbio <brobbio@hotmail.com>
2021-08-09 19:16:01 -0700

    ej 1696

--- 1696/ej_1696.cpp
+++ 1696/ej_1696.cpp
@@ -4,158 +4,44 @@ using namespace std;
 #include<vector>
 #include<iostream>
 #include<tuple>
-/*     int maxResult(vector<int>& nums, int k){
-        int i = 0;
-        int score = 0;
-        while(i<nums.size()){
-            score = score + nums[i];
-                if(i == nums.size()-1){
-                    i = i +1;
-                }else{
-                    if(nums[i+1]>=0){
-                        i = i+1;
-                    }else{
-                        int j = 1;
-                        int prox = 1;
-                        int max = nums[i+1];
-                        while(j<=k){
-                            if(nums[i+j]>0){
-                                prox = j;
-                                j = k+1;
-                            }else{
-                                if(nums[i+j]>=max){
-                                    max = nums[i+j];
-                                    prox = j;
-                                }
-                            }
-                            if(i+j == nums.size()-1){
-                                prox = j;
-                                j = k+1;
-                            }else{
-                                j = j+1;
-                            }
-                        }
-                i = i + prox;
-            }
-        }
-        }
-        return score;
-    } */
+#include<deque>
+#include<queue>
 
+struct CustomCompare
+/* Define a way of comparing tuples of int */
+{
+    bool operator()(const tuple<int,int>& lhs, const tuple<int,int>& rhs)
+    {
+        return get<0>(lhs) < get<0>(rhs);
+    }
+};
 
-/* 
-    int backtracking(vector<int>& nums, vector<int> solucionParcial, int i, int k){
-        int m = nums.size();
-        int mejorValor = -1000;
-        if(i==m-1){
-            int suma = 0;
-            for(int e: solucionParcial){
-                suma = suma + e;
-            }
-            if(suma>mejorValor){
-                mejorValor = suma;
-                vector<int> mejorSolucion = solucionParcial;
-            }
+int maxResult(vector<int>& nums, int k){
+    int n = nums.size();
+    /* Use the above comparison to define a maxHeap of tuples */
+    priority_queue<tuple<int,int>, vector<tuple<int, int>>, CustomCompare> max_dipis;
+    for(int i = 0;i<n;i++){
+        if(i==0){
+            /* Initialize the maxHeap with the tuple <firstElement, 0> */
+            max_dipis.push(make_pair(nums[0],0));
         }else{
-            int j = 1;
-            while(j<=k){
-                if(nums[i+j]>0){
-                    solucionParcial.push_back(nums[i+j]);
-                    return backtracking(nums, solucionParcial,i+j, k);
-                }
-                if(i+j==m-1){
-                    j = k+1;
-                }else{
-                    j++;
-                }
-            } 
-            j = 1; 
-            while(j<=k){
-                solucionParcial.push_back(nums[i+j]);
-                int posible = backtracking(nums, solucionParcial, i+j, k);
-                if(posible>mejorValor){
-                    mejorValor = posible;
-                }
-                solucionParcial.pop_back();
-                if(i+j==m-1){
-                    j = k+1;
-                }else{
-                    j++;
-                }
+            /* Get rid of max tuples that are out of bounds */ 
+            while(i - get<1>(max_dipis.top())>k){
+                max_dipis.pop();
             }
-        }
-        return mejorValor;
-    } */
-
-
-    tuple<int,int> maxResultado(vector<int> nums, int k){
-        int m = nums.size();
-        if(m==1){
-            return make_pair(nums.back(), nums.back()); 
-        }else{
-            vector<tuple<int,int>> res(min(k,m-1));    
-            for(int j = 1;j<=min(k,m-1);j++){
-                tuple<int,int> res_nesimo = maxResultado(std::vector<int>(nums.begin()+j, nums.end()), k);
-                res[j-1] = res_nesimo;
-            }
-            vector<int> caminos;
-            vector<int> caminos_raiz;
-            for(int r = 0; r<min(k,m-1);r++){
-                caminos.push_back(std::get<0>(res[r]));
-                caminos_raiz.push_back(std::get<1>(res[r]));
-            }
-            int maximo = caminos[0];
-            int suma = *(nums.begin());
-            for(int h = 0; h<min(k,m-1); h++){
-                if(caminos[h]>maximo){
-                    maximo = caminos[h];
-                }
-                suma = suma + caminos_raiz[h];
-            }
-            int nuevo = max(maximo, suma);
-
-
-            int caminoDesdeRaiz = *(nums.begin());
-            for(int p = 0; p<min(k,m-1); p++){
-                if(std::get<1>(res[p]) + *(nums.begin())>caminoDesdeRaiz){
-                    caminoDesdeRaiz = std::get<1>(res[p])+*(nums.begin());
-                }
-            }
-            return make_pair(nuevo, caminoDesdeRaiz);
+            /* If I am at the end of the list, return the last element plus maxHeap top */
+            if(i==n-1){
+                    return get<0>(max_dipis.top())+nums[i];
+            }      
+            /* Update the maxHeap with the top of the heap plus the ith element of the list */     
+            int x = get<0>(max_dipis.top());
+            max_dipis.push(make_tuple(nums[i]+x,i));
         }
     }
-
-    int maxResult(vector<int>& nums, int k){
-        return std::get<1>(maxResultado(nums, k));
-    }
-
-/*     int maxResult(vector<int>& nums, int k){
-        int m = nums.size();
-        if(nums.size()<=k+1){
-            int max = nums.back()+nums.front();
-            for(int e: std::vector<int>(nums.begin(), nums.end()-1)){
-                if(e + nums.back()>max){
-                    max = e+nums.back();
-                }
-            }
-            return max;
-        }else{
-            vector<int> paso = std::vector<int>(nums.begin()+m-1-k, nums.end());
-            int finall = maxResult(paso, k);
-            nums.resize(nums.size()-k-1);
-            nums.push_back(finall);
-            return maxResult(nums, k);
-        }
-    } */
-    
-
+    return get<0>(max_dipis.top());
+}
 int main(){
-	vector<int> prueba = {-7622,4131,7439,-2974,-2663,-669,-4494,-9654,8200,-1423,1012,-5399,7978,5597,4156,1269,5752,6413,7845,6468,-2708,-2582,-2473,-1141,2225,4970,1907,-4156,1547,515,-6515,-7948,4723,7159,-208,-9331,2182,2163,4143,-6634,-1341,1365,-3940,1654,-7759,3755,-8914,-2074,-2196,-6471,-1181,-4348,-3286,-8133,-9962,-8414,-2065,-6902,-6140,-4236,2119,9175,6304,-2371,-1397,-6207,-2068,245,8186,-4801,-9801,523,-8516,-8535};
-    int res;
-    if(prueba.back()<0){
-        res = maxResult(prueba, 2)+prueba.back();
-    }else{
-	    res = maxResult(prueba, 2);
-    }
+	vector<int> prueba = {100,-1,-100,-1,100};
+    int res = maxResult(prueba, 2);
 	cout<<res<<endl;
 return 0;}
